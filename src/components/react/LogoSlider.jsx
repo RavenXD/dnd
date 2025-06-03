c857d7f7fd14875ea35336d94ab4e2c23136722e
RavenXD <ummanw@yahoo.com>
2025-06-03 16:48:11 +0500

    update design

--- src/components/react/LogoSlider.jsx
+++ src/components/react/LogoSlider.jsx
@@ -20,18 +20,9 @@ const LogoSlider = () => {
         modules={[FreeMode, Pagination]}
         className="mySwiper"
         breakpoints={{
-          640: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-          768: {
-            slidesPerView: 4,
-            spaceBetween: 40,
-          },
-          1024: {
-            slidesPerView: 5,
-            spaceBetween: 50,
-          },
+          640: { slidesPerView: 2, spaceBetween: 20 },
+          768: { slidesPerView: 4, spaceBetween: 40 },
+          1024: { slidesPerView: 5, spaceBetween: 50 },
         }}
       >
         <SwiperSlide>
